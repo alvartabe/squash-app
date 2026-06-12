9886d489f79d252ba0570ff76ccfd8b8cd4b5f4a
Alvaro Artavia <alvartabe777@gmail.com>
2026-06-12 11:07:07 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     sanity: {
-        projectId: 'ilg43wvz',
+        projectId: '-',
         dataset: 'production',
         useCdn: true,
         apiVersion: '2025-07-04',
